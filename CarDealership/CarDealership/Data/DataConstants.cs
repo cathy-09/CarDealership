2329e59d60ff84cde83d8eb958933de68ad02f08
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2025-02-02 20:51:13 +0200

    Cars model added!
    Cars model added!

---
+++ CarDealership/CarDealership/Data/DataConstants.cs
@@ -0,0 +1,16 @@
+﻿namespace CarDealership.Data
+{
+    public class DataConstants
+    {
+        public class Cars 
+        {
+            public const int BrandMaxLength = 20;
+            public const int ModelMaxLength = 20;
+            public const int FuelTypeMaxLength = 10;
+            public const int KilometersMaxLength = 10;
+            public const int DescriptionMaxLength = 500;
+            public const decimal PriceMin = 0.01m;
+            public const decimal PriceMax = 1000000m;
+        }
+    }
+}
---
+++ CarDealership/CarDealership/Data/Models/Cars.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.VisualBasic;
+using System.ComponentModel.DataAnnotations;
+using static CarDealership.Data.DataConstants.Cars;
+
+namespace CarDealership.Data.Models
+{
+    public class Cars
+    {
+        [Key]
+        public int CarId { get; set; }
+
+        [Required]
+        [MaxLength(BrandMaxLength)]
+        public string Brand { get; set; }
+
+        [Required]
+        [MaxLength(ModelMaxLength)]
+        public string Model { get; set; }
+
+        [Required]
+        public int Year { get; set; }
+
+        [Required]
+        [MaxLength(FuelTypeMaxLength)]
+        public string FuelType { get; set; }
+
+        [Required]
+        [MaxLength(KilometersMaxLength)]
+        public string Kilometers { get; set; }
+
+        [Required]
+        public int HorsePower { get; set; }
+
+        [Required]
+        [MaxLength(DescriptionMaxLength)]
+        public string Description { get; set; }
+
+        [Required]
+        [Range((double)DataConstants.Cars.PriceMin, (double)DataConstants.Cars.PriceMax)]
+        public decimal Price { get; set; }
+
+        //public ICollection<DealershipCar> DealershipCars { get; set; }
+    }
+}
