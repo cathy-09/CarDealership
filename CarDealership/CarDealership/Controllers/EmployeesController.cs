2df494cce2273b873c0b8430c2a7665bbeb27cfc
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2025-02-08 22:47:07 +0200

    EmployeesController

---
+++ CarDealership/CarDealership/Controllers/EmployeesController.cs
@@ -0,0 +1,33 @@
+﻿using CarDealership.Data;
+using CarDealership.Models.Employees;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace CarDealership.Controllers
+{
+    public class EmployeesController : Controller
+    {
+        private readonly CarDealershipDbContext _context;
+
+        public EmployeesController(CarDealershipDbContext carDealershipDbContext)
+        {
+            _context = carDealershipDbContext;
+        }
+        public async Task<IActionResult> All()
+        {
+            var employees = await _context.Employees
+                .Include(e => e.Dealerships)
+                .Select(e => new EmployeeViewModel()
+                {
+                    EmployeeId = e.EmployeeId,
+                    FullName = e.FirstName + " " + e.LastName,
+                    HireDate = e.HireDate,
+                    DealershipName = e.Dealerships.Name 
+                })
+                .ToListAsync();
+
+            return View(employees);
+        }
+
+    }
+}
