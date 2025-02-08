e8babe02534dbb29a91ccd76222a10e8b05b27fa
Katerina Dimitrova <158329994+cathy-09@users.noreply.github.com>
2025-02-08 22:16:09 +0200

    Place method Edit in CarController

--- CarDealership/CarDealership/Controllers/CarController.cs
+++ CarDealership/CarDealership/Controllers/CarController.cs
@@ -92,5 +92,57 @@ namespace CarDealership.Controllers
             await _context.SaveChangesAsync();
             return RedirectToAction("All", "Car");
         }
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var car = await _context.Cars.FindAsync(id);
+            if (car == null)
+            {
+                return NotFound();
+            }
+
+            return View(car);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("CarId,CarImageURL,Brand,Model,Year,FuelType,Kilometers,HorsePower,Description,Price")] Car car)
+        {
+            if (id != car.CarId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(car);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!CarExists(car.CarId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(car);
+        }
+
+        private bool CarExists(int id)
+        {
+            return _context.Cars.Any(e => e.CarId == id);
+        }
     }
 }
