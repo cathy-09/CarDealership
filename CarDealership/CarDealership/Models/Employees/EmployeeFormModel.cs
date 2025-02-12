8c674a6f6b53596de711d2715a54ff41a3e82122
Peter Baklarov <p.baklarov@gmail.com>
2025-02-12 22:26:09 +0200

    Fix

---
+++ CarDealership/CarDealership/Models/Employees/EmployeeFormModel.cs
@@ -0,0 +1,21 @@
+﻿using CarDealership.Models.Dealerships;
+
+namespace CarDealership.Models.Employees
+{
+    public class EmployeeFormModel
+    {
+        public EmployeeFormModel()
+        {
+        }
+
+        public IEnumerable<DealershipsModel> Dealerships { get; set; }
+        public string FirstName { get; internal set; }
+        public string LastName { get; internal set; }
+        public string Email { get; internal set; }
+        public string PhoneNumber { get; internal set; }
+        public DateTime HireDate { get; internal set; }
+        public decimal Salary { get; internal set; }
+        public int DealershipId { get; internal set; }
+        public string UserId { get; internal set; }
+    }
+}
